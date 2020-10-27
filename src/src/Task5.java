58a823e723659e482dd52edf1163002131ef36d6
IrinaDoroftei <73463367+IrinaDoroftei@users.noreply.github.com>
2020-10-27 19:23:06 +0200

    Revert "Task7.6:"

--- src/src/Task5.java
+++ src/src/Task5.java
@@ -2,10 +2,4 @@ public class Task5 {
     public static void main(String args[]){
 
     System.out.println("Good Afternoon");}
-
-    static void myNew() {
-        System.out.println("Test3");
-    }
-    static void myNew2
-    System.out.println("Test2");}
-            }
+}
