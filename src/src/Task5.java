77c4acfe644ec44ec98f84ee35942a2a37a50d68
IDoroftei <irina.doroftei@endava.com>
2020-10-27 17:54:02 +0200

    Task7.6: * Added updated methods

--- src/src/Task5.java
+++ src/src/Task5.java
@@ -2,4 +2,10 @@ public class Task5 {
     public static void main(String args[]){
 
     System.out.println("Good Afternoon");}
-}
+
+    static void myNew() {
+        System.out.println("Test3");
+    }
+    static void myNew2
+    System.out.println("Test2");}
+            }
